54540666c3e70b1b32099f44ef30650f5b68c358
oleggr <oleg.gr@outlook.com>
2021-06-27 04:34:27 +0300

    logger added

--- app.lua
+++ app.lua
@@ -1,13 +1,18 @@
 #!/usr/bin/env tarantool
 
-box.cfg{}
+box.cfg{
+	log = 'tarantool.log',
+	log_format='plain'
+}
+
 box.once('schema', function()
 	box.schema.create_space('items')
 	box.space.items:create_index('primary', { type = 'hash',
 		parts = {1, 'str'} })
 end)
 
-function validate_item(item)
+function validate_item_body(item)
+	--todo: fix body validation
 	return true
 end
 
@@ -21,13 +26,16 @@ function post_item_handler(req)
 	local my_table = req:json()
 
 	if check_element_exist(my_table['key']) then
+		log.error('post_item_handler - key already exist')
 		return {status = 409, body="key already exist"}
 	end
 
-	if not validate_item(my_table['key']) then
+	if not validate_item_body(my_table['value']) then
+		log.error('post_item_handler - body incorrect')
 		return {status = 400, body="body incorrect"}
 	end
 
+	log.info('post_item_handler - key %s inserted', my_table['key'])
 	box.space.items:insert({my_table['key'], my_table['value']})
 	return {status = 200}
 end
@@ -37,13 +45,16 @@ function put_item_handler(req)
 	local my_table = req:json()
 
 	if not check_element_exist(id) then
+		log.error('put_item_handler - key not exist')
 		return {status = 404, body="key not exist"}
 	end
 
-	if not validate_item(id) then
+	if not validate_item_body(my_table['value']) then
+		log.error('put_item_handler - body incorrect')
 		return {status = 400, body="body incorrect"}
 	end
 
+	log.info('put_item_handler - key %s updated', id)
 	box.space.items:update({id}, {{'=', 2, my_table['value']}})
 	return {status = 200}
 end
@@ -52,6 +63,7 @@ function get_item_handler(req)
 	local id = req:stash('id')
 	local res = box.space.items:select({id})
 	if table.getn(res)==0 then
+		log.error('get_item_handler - key not exist')
 		return {status = 404, body="key not exist"}
 	end
 	return req:render{status = 200, json=res}
@@ -60,11 +72,15 @@ end
 function delete_item_handler(req)
 	local id = req:stash('id')
 	if not check_element_exist(id) then
+		log.error('delete_item_handler - key not exist')
 		return {status = 404, body="key not exist"}
 	end
+	log.info('delete_item_handler - key %s deleted', id)
 	box.space.items:delete({id})
 	return {status = 200}
 end
+
+log = require('log')
 
 local httpd = require('http.server')
 local server = httpd.new('0.0.0.0', 8080)
