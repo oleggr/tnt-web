e24dd15de31736155a42cc1318be35a6f92cac75
oleggr <oleg.gr@outlook.com>
2021-06-27 04:10:15 +0300

    functionality updated

--- app.lua
+++ app.lua
@@ -2,8 +2,8 @@
 
 box.cfg{}
 box.once('schema', function()
-	box.schema.create_space('hosts')
-	box.space.hosts:create_index('primary', { type = 'hash',
+	box.schema.create_space('items')
+	box.space.items:create_index('primary', { type = 'hash',
 		parts = {1, 'str'} })
 end)
 
@@ -11,31 +11,59 @@ function validate_item(item)
 	return true
 end
 
+function check_element_exist(key)
+	local res = box.space.items:select({key})
+	if table.getn(res)==0 then return false end
+	return true
+end
+
 function post_item_handler(req)
 	local my_table = req:json()
-	box.space.hosts:insert({my_table['key'], my_table['value']})
+
+	if check_element_exist(my_table['key']) then
+		return {status = 409, body="key already exist"}
+	end
+
+	if not validate_item(my_table['key']) then
+		return {status = 400, body="body incorrect"}
+	end
+
+	box.space.items:insert({my_table['key'], my_table['value']})
 	return {status = 200}
 end
 
 function put_item_handler(req)
 	local id = req:stash('id')
 	local my_table = req:json()
-	box.space.hosts:update({id}, {{'=', 2, my_table['value']}})
+
+	if not check_element_exist(id) then
+		return {status = 404, body="key not exist"}
+	end
+
+	if not validate_item(id) then
+		return {status = 400, body="body incorrect"}
+	end
+
+	box.space.items:update({id}, {{'=', 2, my_table['value']}})
 	return {status = 200}
 end
 
 function get_item_handler(req)
 	local id = req:stash('id')
-	local res = box.space.hosts:select(id)
-	return tostring(res)
-	--return {status = 200}
+	local res = box.space.items:select({id})
+	if table.getn(res)==0 then
+		return {status = 404, body="key not exist"}
+	end
+	return req:render{status = 200, json=res}
 end
 
 function delete_item_handler(req)
 	local id = req:stash('id')
-	box.space.hosts:delete({id})
-	return id
-	--return {status = 200}
+	if not check_element_exist(id) then
+		return {status = 404, body="key not exist"}
+	end
+	box.space.items:delete({id})
+	return {status = 200}
 end
 
 local httpd = require('http.server')
